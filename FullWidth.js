08ab6aba18b36c4d6502c4e54170815d5712a755
Varun Vachhar <varunvachhar@gmail.com>
2018-11-18 16:41:23 -0500

    📝

---
+++ FullWidth.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { color } from 'styled-system';
+
+const FullWidth = styled.div(
+  [],
+  {
+    width: '100%',
+  },
+);
+
+FullWidth.defaultProps = {
+};
+
+export default FullWidth;
